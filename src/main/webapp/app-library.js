067cbaec828ab10931a41c463c4dc4a390ed0d98
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2020-07-26 22:35:30 +0300

    a few modifications to make the code look better

--- src/main/webapp/app-library.js
+++ src/main/webapp/app-library.js
@@ -1,36 +1,39 @@
 var TracomAcademy = TracomAcademy || {};
 
-TracomAcademy.contentRender = "content";
+const contentRender = "content";
 
 TracomAcademy.Grid = function(){
-  var me = this;
+  const me = this;
 
-  var table = "<table border=\"1\"><tr><th>Name</th><th>Code<th></tr>";
+  let table = `<table border=1><tr><th>Name</th><th>Code<th></tr>`;
 
   me.store.forEach(field => {
-      table += `<label for=\"${field.id}\">${field.label}</label><br> <input type=\"${field.type}\" id=\"${field.id}\" name=\"${field.name}\"><br>`;
+     table += `<tr><td>${field.name}</td><td>${field.code}<td></tr>`;
    });
 
    table += "</table>";
 
-   document.getElementById(me.contentRender).innerHTML = table;
+   document.getElementById(contentRender).innerHTML = table;
 
 }
 
 TracomAcademy.Form = function(){
-   var me = this;
+   const me = this;
 
-   var form = "<form action=\"/action_page.php\">";
+   let form = "<form>";
+   let submit = `<br><input type="submit" id="submit" name="submit"/><br>`;
 
    me.form.forEach(field => {
-      form += `<label for=\"${field.id}\">${field.label}</label><br> <input type=\"${field.type}\" id=\"${field.id}\" name=\"${field.name}\"><br>`;
+      form += `<label for="${field.id}">${field.label}</label><br> 
+      <input type="${field.type}" id="${field.id}" name="${field.name}" /><br>
+      `;
    });
+   
+   form += `${submit}</form>`;
 
-   form +=  "</form>";
-
-   document.getElementById(me.contentRender).innerHTML = form;
+   document.getElementById(contentRender).innerHTML = form;
 
 }
 
 
-/// {name: "name",fieldLabel: "
+export default TracomAcademy;
